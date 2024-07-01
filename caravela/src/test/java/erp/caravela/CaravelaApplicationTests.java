4e7980e67febad068a5fcc6d422a5ee04e13eb9f
Julio Dias <julio.dias@logone.com.br>
2024-07-01 19:35:01 -0300

    primeiro commit

---
+++ caravela/src/test/java/erp/caravela/CaravelaApplicationTests.java
@@ -0,0 +1,13 @@
+package erp.caravela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravelaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
